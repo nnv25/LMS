6e5a19cb22d7e621a90f16b77cc6f24ce7b4e5c6
nnv25 <nnv25@mail.ru>
2025-03-04 20:34:52 +0900

    040325

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -4,12 +4,16 @@ import { AppContext } from "../../context/AppContext";
 import Loading from "../../components/student/Loading";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
+import Footer from "../../components/student/Footer";
+import YouTube from "react-youtube";
 
 const CourseDetails = () => {
   const { id } = useParams();
 
   const [courseData, setCourseData] = useState(null);
   const [openSections, setOpenSections] = useState({});
+  const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false);
+  const [playerData, setPlayerData] = useState(null);
 
   const {
     allCourses,
@@ -27,7 +31,7 @@ const CourseDetails = () => {
 
   useEffect(() => {
     fetchCourseData();
-  });
+  }, [allCourses]);
 
   const toggleSection = (index) => {
     setOpenSections((prev) => ({ ...prev, [index]: !prev[index] }));
@@ -125,7 +129,16 @@ const CourseDetails = () => {
                             <p>{lecture.lectureTitle}</p>
                             <div className="flex gap-2">
                               {lecture.isPreviewFree && (
-                                <p className="text-blue-500 cursor-pointer">
+                                <p
+                                  onClick={() =>
+                                    setPlayerData({
+                                      videoId: lecture.lectureUrl
+                                        .split("/")
+                                        .pop(),
+                                    })
+                                  }
+                                  className="text-blue-500 cursor-pointer"
+                                >
                                   Preview
                                 </p>
                               )}
@@ -159,7 +172,15 @@ const CourseDetails = () => {
         </div>
         {/* right column */}
         <div className="max-w-[424px] z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]">
-          <img src={courseData.courseThumbnail} alt="" />
+          {playerData ? (
+            <YouTube
+              videoId={playerData.videoId}
+              opts={{ playerVars: { autoplay: 1 } }}
+              iframeClassName="w-full aspect-video"
+            />
+          ) : (
+            <img src={courseData.courseThumbnail} alt="" />
+          )}
           <div className="p-5">
             <div className="flex items-center gap-2">
               <img
@@ -192,10 +213,36 @@ const CourseDetails = () => {
                 <img src={assets.star} alt="star icon" />
                 <p>{calculateRating(courseData)}</p>
               </div>
+              <div className="h-4 w-px bg-gray-500/40"></div>
+              <div className="flex items-center gap-1">
+                <img src={assets.time_clock_icon} alt="clock icon" />
+                <p>{calculateCourseDuration(courseData)}</p>
+              </div>
+              <div className="h-4 w-px bg-gray-500/40"></div>
+              <div className="flex items-center gap-1">
+                <img src={assets.lesson_icon} alt="clock icon" />
+                <p>{calculateNoOfLectures(courseData)}lessons</p>
+              </div>
+            </div>
+            <button className="md:mt-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium">
+              {isAlreadyEnrolled ? "Already Enrolled" : "Enroll Now"}
+            </button>
+            <div className="pt-6">
+              <p className="md:text-xl text-lg font-medium text-gray-800">
+                What's in the course?
+              </p>
+              <ul className="ml-4 pt-2 text-sm md:text-default list-disc text-gray-500">
+                <li>Lifetime access with free updates.</li>
+                <li>Step-by-step, hands-on project guidance.</li>
+                <li>Downloadable resources and source code</li>
+                <li>Quizzes to test your knowledge.</li>
+                <li>Certificate of completion.</li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </>
   ) : (
     <Loading />
--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
 
 const MyEnrollments = () => {
+  const { enrolledCourses, calculateCourseDuration } = useContext(AppContext);
   return (
-    <div>MyEnrollments</div>
-  )
-}
+    <>
+      <div className="md:px-36 px-8 pt-10">
+        <h1 className="text-2xl font-semibold">My Enrollments</h1>
+        <table className="md:table-auto table-fixed w-full overflow-hidden border mt-10">
+          <thead className="text-gary-900 border-b border-gray-500/20 text-sm text-left max-sm:hidden">
+            <tr>
+              <th className="px-4 py-3 font-semibold truncate">Course</th>
+              <th className="px-4 py-3 font-semibold truncate">Duration</th>
+              <th className="px-4 py-3 font-semibold truncate">Completed</th>
+              <th className="px-4 py-3 font-semibold truncate">Status</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-700">
+            {enrolledCourses.map((course, index) => (
+              <tr key={index} className="border-b border-gray-500/20">
+                <td className="md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3">
+                  <img
+                    src={course.courseThumbnail}
+                    alt=""
+                    className="w-14 sm:w-24 md:w-28"
+                  />
+                  <div>
+                    <p>course.courseTitle</p>
+                  </div>
+                </td>
+                <td>{calculateCourseDuration(course)}</td>
+                <td>
+                  4 / 10 <span>Lectures</span>
+                </td>
+                <td>
+                  <button>On Going</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default MyEnrollments
+export default MyEnrollments;
