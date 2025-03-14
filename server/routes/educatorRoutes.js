cf6fcb46b0ef14eb816e4686a199ee0b247d3b1b
Пользователь <nnv25@mail.ru>
2025-03-14 14:31:10 +0900

    140325

---
+++ server/routes/educatorRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { addCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorController.js'
+
+const educatorRouter = express.Router()
+
+//Add Educator Role
+educatorRouter.get('/update-role', updateRoleToEducator)
+educatorRouter.post('/add-course', UploadStream.single('image'), protectEducator, addCourse)
+educatorRouter.get('/courses', protectEducator, getEducatorCourses)
+educatorRouter.get('/dashboard', protectEducator, educatorDashboardData)
+educatorRouter.get('/enrolled-students', protectEducator, getEnrolledStudentsData)
+
+export default educatorRouter;
